55766596aba06c2908a02496b0e2296560eb8c5e
Ilya <khalko.is@phystech.edu>
2022-12-26 08:33:55 +0300

    some changes of classes structure

--- matrix.h
+++ matrix.h
@@ -10,13 +10,11 @@ public:
 	matr(int rows, int columns, double** data);
 	~matr();
 	void print_matr();
+	void create_matr();
 	double** get_matr();
 	int get_rows();
 	int get_columns();
 	void set_matr(int rows, int columns, double** data);
-
-	friend matr operator+(const matr& a, const matr& b);
-	friend matr operator-(const matr& a, const matr& b);
 	matr& operator = (const matr& a);
 private:
 	int columns;
--- vector.h
+++ vector.h
@@ -15,12 +15,15 @@ public:
 	double* get_vect();
 	int get_size();
 	void set_vect(int size, double* data);
-
+	double vector_norm();
+	
 	friend class matr;
 	friend vect operator+(const vect& a, const vect& b);
 	friend vect operator-(const vect& a, const vect& b);
 	vect& operator = (const vect& a);
 	friend vect mult(matr& a, vect& b);
+	void mult_double(double a);
+	void create_vector();
 private:
 	int size;
 	double* data;

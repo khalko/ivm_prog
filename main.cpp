b18527e2baf02600fc89358df0bb782d2cd313a4
Ilya <khalko.is@phystech.edu>
2022-12-26 08:31:37 +0300

    iteration method realisation + functionality testing

--- main.cpp
+++ main.cpp
@@ -5,60 +5,92 @@
 
 int main()
 {
-	int col = 3;
-	int row = 3;
-	double** matritsa = new double* [row];
+	/*matr matrix1;
+	matrix1.create_matr();
+	matrix1.print_matr();
 
-	for (int i = 0; i < row; ++i)
-	{
-		matritsa[i] = new double[col];
-	}
-
-	for (int i = 0; i < row; ++i)
-	{
-		for (int j = 0; j < col; ++j)
-		{
-			std::cin >> matritsa[i][j];
-		}
-	}
-	/*double* vector1 = new double[size1];
-	double* vector2 = new double[size2];
-	for (int i = 0; i < size1; ++i)
-	{
-		std::cout << "Give me " << i << " coordinate";
-		std::cin >> vector1[i];
-	}
-	for (int i = 0; i < size2; ++i)
-	{
-		std::cout << "Give me " << i << " coordinate";
-		std::cin >> vector2[i];
-	}
-	vect a(size1);
-	vect b(size2);
-	vect c(size1);
-	a.set_vect(size1, vector1);
-	b.set_vect(size2, vector2);
-
-	c = a + b;
+	vect a;
+	a.create_vector();
 	a.print_vect();
-	b.print_vect();
-	c.print_vect();
-	return 0;*/
 
-	matr matras(row, col, matritsa);
-	double* vector1 = new double[col];
-	for (int i = 0; i < col; ++i)
-	{
-		std::cout << "Give me " << i << " coordinate";
-		std::cin >> vector1[i];
-	}
-	vect a(col, vector1);
-	vect c = mult(matras, a);
-	matras.print_matr();
+	vect c = mult(matrix1, a);
+	c.print_vect();
+	
+	c.mult_double(2.0);
 	c.print_vect();
 
-	vect sum(row);
-	sum = a + c;
-	sum.print_vect();
 
+
+	matr matrix2;
+	matrix2 = matrix1;
+	matrix2.print_matr();*/
+
+    matr A;
+    A.create_matr();
+    int row = A.get_rows();
+    int col = A.get_columns();
+
+    vect b;
+    b.create_vector();
+
+    double** alpha_data = new double* [row];
+    for (int i = 0; i < row; ++i)
+    {
+        alpha_data[i] = new double[col];
+    }
+
+    for (int i = 0; i < row; ++i)
+    {
+        for (int j = 0; j < col; ++j)
+        {
+            if (i != j)
+            {
+                alpha_data[i][j] = (-1) * A.get_matr()[i][j] / A.get_matr()[i][i];
+            }
+            else
+            {
+                alpha_data[i][j] = 0;
+            }  
+        }
+    }
+    matr alpha(row, col, alpha_data);
+
+    double* betta_data = new double[row];
+    for (int i = 0; i < row; ++i)
+    {
+        betta_data[i] = b.get_vect()[i] / A.get_matr()[i][i];
+    }
+    vect betta(row, betta_data);
+
+    vect x_i = b;
+    vect x_i1(row);
+
+    double epsilon;
+
+    std::cout << "Give an epsilon: ";
+    std::cin >> epsilon;
+
+    int iter = 0;
+    while ((x_i1 - x_i).vector_norm() > epsilon)
+    {
+        x_i = x_i1;
+        x_i1 = betta + mult(alpha, x_i);
+        ++iter;
+        std::cout << iter << " iteration result" << std::endl;
+        x_i1.print_vect();
+        if ((x_i1 - x_i).vector_norm() <= epsilon)
+        {
+            std::cout << "Final result ";
+        }
+        std::cout << std::endl;
+    }
+
+    for (int i = 0; i < row; ++i)
+    {
+        delete[] alpha_data[i];
+    }
+    delete[] alpha_data;
+
+	//std::cout << "I'm heere! " << std::endl;
+	return 0;
 }

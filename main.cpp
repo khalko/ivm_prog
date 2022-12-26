e606f70399398250e0b38c7ff940c5abfc95f0ca
Ilya <khalko.is@phystech.edu>
2022-12-26 07:46:13 +0300

    new main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,16 @@
+#include "main.h"
 #include <iostream>
+
+void hello_world(int year)
+{
+	std::cout << "Hello world of " << year << "!" << std::endl;
+}
 
 int main()
 {
-    std::cout << "Hello world!";
+	int year;
+	std::cout << "What year is it?" << std::endl;
+	std::cin >> year;
+    hello_world(year);
     return 0;
 }
